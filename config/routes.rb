4a72a3ed5bd047fa67f49dbbade0c8b90c166ef4
MAYUMI816 <cova076@gmail.com>
2020-02-10 08:08:04 +0000

    Finish top-page【勤怠2章】

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
-
+  #get 'static_pages/top'
+  root 'static_pages#top' #get 'static_pages/top'　URL/static_pages/topから〜comで表示　
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
