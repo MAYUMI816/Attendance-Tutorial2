2c9b6df2b8bf6411305dd1a2f46d641c0072a767
MAYUMI816 <cova076@gmail.com>
2020-02-18 07:43:41 +0000

    10. 3まで終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info] #8. 5. 2 destroyアクション
   before_action :correct_user, only: [:edit, :update] # 8. 2. 2
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info] # 8. 5. 2 destroyアクション
-  
+  before_action :set_one_month, only: :show #10.3
   
 
   # 8. 4 全てのユーザーを表示するページ
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
   def show
     # @user = User.find(params[:id]) # 5. 2findメソッドを使ってユーザーオブジェクトを取得し、インスタンス変数に代入
     # 8. 2. 2で削除
-    @first_day = Date.current.beginning_of_month
-    @last_day = @first_day.end_of_month
+    # 10.3 でま削除@first_day = Date.current.beginning_of_month
+    #10.3 でま削除@last_day = @first_day.end_of_month
   # @first_dayで、当日を取得するためDate.currentを使用
   # これにRailsのメソッドbeginning_of_monthを繋げる当月の初日を取得可能  
   end
